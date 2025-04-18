43c5974434d1c5a07e739938e4a813d34d3b61ae
GayatriLounganiShorthillsAI <Gayatri.Loungani@shorthills.ai>
2025-04-18 11:52:58 +0530

    generic relationship

--- storefront/tags/models.py
+++ storefront/tags/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from store.models import Product
 # Create your models here.
+
+class Tag(models.Model):
+        label = models.CharField(max_length=255)
+
+
+class TaggedItem():
+# what tag applied to what object
+        tag =models.ForeignKey(Tag, on_delete=models.CASCADE)
+        product = models.ForeignKey(Product)
