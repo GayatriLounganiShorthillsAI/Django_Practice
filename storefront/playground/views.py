186214d8dc890f913c92c13b01b5a44f57a47e30
GayatriLounganiShorthillsAI <Gayatri.Loungani@shorthills.ai>
2025-04-23 10:58:53 +0530

    limiting result

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -37,8 +37,17 @@ def say_hello(request):
 
         # *******sorting*******
         # queryset = Product.objects.order_by('unit_price','-title').reverse()
-        product = Product.objects.order_by('unit_price')[0]    # order by return the query set 
-        product = Product.objects.earliest('unit_price')   #returns the top element
-        product = Product.objects.latest('unit_price')   #sort in desecending order and return the last element
-        return render(request, 'hello.html', {'name' : 'Gayatri', 'product' : product})
+        # product = Product.objects.order_by('unit_price')[0]    # order by return the query set 
+        # product = Product.objects.earliest('unit_price')   #returns the top element
+        # product = Product.objects.latest('unit_price')   #sort in desecending order and return the last element
+   
+
+        # ********limiting result***********
+        # 0,1,2,3,4 excluding top 5
+        queryset = Product.objects.all()[5:10]
+  
+
+        # *****selecting fields to query******
+
+        return render(request, 'hello.html', {'name' : 'Gayatri', 'products' : list(queryset)})
  
