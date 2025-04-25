820a75ed0fab0a97a5e5b31ad8e86b8665e98b60
GayatriLounganiShorthillsAI <Gayatri.Loungani@shorthills.ai>
2025-04-25 16:45:04 +0530

    overridding base query set

--- storefront/store/admin.py
+++ storefront/store/admin.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
 from . import models
+from django.db.models.aggregates import Count
 # Register your models here.
 
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-        list_display = ['title', 'unit_price', 'inventory_status']
+        list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
         list_editable = ['unit_price']
         list_per_page = 10
+        list_select_related = ['collection']
+
+        def collection_title(self, product):
+                return product.collection.title
 
 # ****** ADDING COMPUTED COLUMNS*****
         @admin.display(ordering='inventory')   # this decorator allows us to sort the computed column
@@ -16,6 +21,8 @@ class ProductAdmin(admin.ModelAdmin):
                         return 'Low'
                 return 'OK'
         
+
+
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
         list_display = ['first_name', 'last_name', 'email', 'phone', 'birth_date', 'membership']
@@ -23,7 +30,24 @@ class CustomerAdmin(admin.ModelAdmin):
         ordering = ['first_name' , 'last_name']
         list_per_page = 10
 
-admin.site.register(models.Collection)
 
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+        list_display = ['id', 'placed_at', 'customer']
+      
+
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+        list_display = ['title', 'products_count']
+
+        @admin.display(ordering='products_count')
+        def products_count(self, collection):
+                return collection.products_count
+        
+        def get_queryset(self, request):
+                return super().get_queryset(request).annotate(
+                        products_count = Count('product')
+                )
 
 # admin.site.register(models.Product, ProductAdmin)
